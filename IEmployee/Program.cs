60c6a57fb70db7979216cf009dba37c69531ac7f
denlisk <160236126+denlisk@users.noreply.github.com>
2024-04-07 20:55:50 +0700

    Update Program.cs

--- IEmployee/Program.cs
+++ IEmployee/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main()
         {
+            // 04.04.2024
             EmployeeList<IEmployee> guys = new EmployeeList<IEmployee>();
             Manager vasyan = new Manager("Вася", 20, IEmployee.GradeLevel.Nobody);
             Engineer kolyan = new Engineer("Коля", 54, IEmployee.GradeLevel.Director);
