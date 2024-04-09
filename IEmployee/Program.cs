8f048f38a095156c80d131f147096c36a8b4483e
denlisk <160236126+denlisk@users.noreply.github.com>
2024-04-09 12:27:48 +0700

    update IEmployee/Program.cs

--- IEmployee/Program.cs
+++ IEmployee/Program.cs
@@ -1,18 +1,17 @@
-﻿namespace InterfaceEmployee
+namespace InterfaceEmployee
 {
-    class Program
-    {
-        static void Main()
-        {
-            // 04.04.2024
-            EmployeeList<IEmployee> guys = new EmployeeList<IEmployee>();
-            Manager vasyan = new Manager("Вася", 20, IEmployee.GradeLevel.Nobody);
-            Engineer kolyan = new Engineer("Коля", 54, IEmployee.GradeLevel.Director);
-            guys.Add(vasyan);
-            guys.Add(kolyan);
-            guys.Print();
-            guys.Remove(vasyan);
-            guys.Print();
-        }
-    }
+	class Program
+	{
+		static void Main() {
+			// 04.04.2024
+			EmployeeList < IEmployee > guys = new EmployeeList < IEmployee > ();
+			Manager vasyan = new Manager("Вася", 20, IEmployee.GradeLevel.Nobody);
+			Engineer kolyan = new Engineer("Коля", 54, IEmployee.GradeLevel.Director);
+			guys.Add(vasyan);
+			guys.Add(kolyan);
+			guys.Print();
+			guys.Remove(vasyan);
+			guys.Print();
+		}
+	}
 }
